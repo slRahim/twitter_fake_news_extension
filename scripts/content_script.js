335cc738cbdc4267b3377a0148a9b5dc8766d29b
abderrahim slimani <abderrahim.slimani@univ-constantine2.dz>
2022-12-13 12:08:53 +0100

    create functions

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -1,25 +1,56 @@
 window.addEventListener('load', function () {
-
-    setTimeout(() => {
-        console.log("Tweets")
-        const articles = document.getElementsByTagName("article");
-        
-        var button = document.createElement("Button");
-        button.innerHTML = "FAKE/TRUE";
-        button.style = "top:8%;right:8%;position:fixed;";
-        button.onclick = ()=>{
-            this.alert("this tweet is ??????")
-        }
-        articles[0].appendChild(button);
-        
-        for (var i = 0; i < articles.length; i++) {
-            console.log(articles[i].querySelector('[data-testid="tweetText"]>span').innerText);
-        }
+    console.log("hello")
 
 
-    }, 7000)
+
 
 
 
 })
+
+// ******************************************part I*****************************************************************
+function tweet_text() {
+    setTimeout(() => {
+        console.log("Tweets")
+        const articles = document.getElementsByTagName("article");
+        
+        for (var i = 0; i < articles.length; i++) {
+            console.log(articles[i].querySelector('[data-testid="tweetText"]>span').innerText);
+
+        }
+
+
+    }, 7000)
+}
+
+function tweet_metadata() {
+    // nbr like,retweet, comment
+
+}
+
+function user_data() {
+    // user
+}
+
+function btn_tweet_test() {
+    // button
+
+    var button = document.createElement("Button");
+    button.innerHTML = "FAKE/TRUE";
+    button.style = "top:8%;right:8%;position:fixed;";
+    button.onclick = () => {
+        this.alert("this tweet is ??????")
+    }
+}
+
+// ******************************part II*******************************************************************
+function test_edition(){
+
+}
+
+function btn_test_edition(){
+
+}
+
+
 
