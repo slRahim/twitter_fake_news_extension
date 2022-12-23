89ea9ba24ebee65b6c0a9d80cdb17814ef38c8a6
abderrahim slimani <abderrahim.slimani@univ-constantine2.dz>
2022-12-23 20:15:05 +0100

    add btn test & get tweet text in scroll

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -1,27 +1,83 @@
 window.addEventListener('load', function () {
     console.log("hello")
-
-
-
-
+    tweet_text()
+    // btn_tweet_test()
 
 
 })
 
 // ******************************************part I*****************************************************************
 function tweet_text() {
-    setTimeout(() => {
-        console.log("Tweets")
+    let list_ids = []
+    let list_text = []
+    console.log("Extension is loading...")
+    this.setTimeout(() => {
+        console.log("load is done")
+
         const articles = document.getElementsByTagName("article");
-        
-        for (var i = 0; i < articles.length; i++) {
-            console.log(articles[i].querySelector('[data-testid="tweetText"]>span').innerText);
+
+
+        for (let i = 0; i < articles.length; i++) {
+            let tweet_id = articles[i].querySelector('[data-testid="tweetText"]').getAttribute("id")
+
+            if (!list_ids.includes(tweet_id)) {
+                list_ids.push(tweet_id);
+                list_text.push(document.getElementById(tweet_id).firstChild.innerText);
+
+                let button = document.createElement("Button");
+                button.innerHTML = "TEST";
+                button.style = "top:8%;right:8%;position:fixed;";
+                button.setAttribute("id", "btn_fk_" + tweet_id)
+                button.setAttribute("type", "button")
+                button.addEventListener("click", function () {
+                    console.log(list_text[list_ids.indexOf(tweet_id)])
+                }, false)
+                document.getElementById(tweet_id).appendChild(button)
+            }
 
         }
 
+        this.window.onscroll = function () {
+
+            if (this.oldScroll > this.scrollY) {
+                // true up
+                console.log("scroll up")
+            } else {
+                //false down
+                const articles = document.getElementsByTagName("article");
+
+
+                for (let i = 0; i < articles.length; i++) {
+                    let tweet_id = articles[i].querySelector('[data-testid="tweetText"]').getAttribute("id")
+
+                    if (!list_ids.includes(tweet_id)) {
+                        list_ids.push(tweet_id);
+                        list_text.push(document.getElementById(tweet_id).firstChild.innerText);
+
+                        let button = document.createElement("Button");
+                        button.innerHTML = "TEST";
+                        button.style = "top:8%;right:8%;position:fixed;";
+                        button.setAttribute("id", "btn_fk_" + tweet_id)
+                        button.setAttribute("type", "button")
+                        button.addEventListener("click", function () {
+                            console.log(list_text[list_ids.indexOf(tweet_id)])
+                        }, false)
+                        document.getElementById(tweet_id).appendChild(button)
+                    }
+
+                }
+
+
+            }
+            this.oldScroll = this.scrollY;
+        }
 
     }, 7000)
+
+
 }
+
+
 
 function tweet_metadata() {
     // nbr like,retweet, comment
@@ -32,23 +88,14 @@ function user_data() {
     // user
 }
 
-function btn_tweet_test() {
-    // button
 
-    var button = document.createElement("Button");
-    button.innerHTML = "FAKE/TRUE";
-    button.style = "top:8%;right:8%;position:fixed;";
-    button.onclick = () => {
-        this.alert("this tweet is ??????")
-    }
-}
 
 // ******************************part II*******************************************************************
-function test_edition(){
+function test_edition() {
 
 }
 
-function btn_test_edition(){
+function btn_test_edition() {
 
 }
 
